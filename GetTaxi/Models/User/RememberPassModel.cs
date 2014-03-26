9d95093ab4d63b07fc6fa11a71f0951fa5e29dea
Witali Stinski <vitsv88@gmail.com>
2014-03-26 18:43:19 +0100

    Logowanie, przywrocenia hasla - nowe pliki

---
+++ GetTaxi/Models/User/RememberPassModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models.User
+{
+    public class RemeberPassModel
+    {
+        [Required(ErrorMessage = "Numer telefonu jest wymagany")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "Numer telefonu powinien składać się z 9 cyfr, bez spacii")]
+        [StringLength(40)]
+        public string Phone { get; set; }
+    }
+}
